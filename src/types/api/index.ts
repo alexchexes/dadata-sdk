01667b53351a4e508b444b228ab677b79209fe39
alexchexes <alexchexes@gmail.com>
2025-04-02 12:02:51 +0100

    fix(types): add all api types to types/api/index.ts

--- src/types/api/index.ts
+++ src/types/api/index.ts
@@ -1,21 +1,45 @@
 export * from './api-common.types.ts';
 
 export * from './request/endpoints.types.ts';
+
 export * from './request/suggest-payload-address.types.ts';
 export * from './request/suggest-payload-bank.types.ts';
 export * from './request/suggest-payload-fias.types.ts';
 export * from './request/suggest-payload-fio.types.ts';
+export * from './request/suggest-payload-fms_unit.types.ts';
+export * from './request/suggest-payload-fns_unit.types.ts';
+export * from './request/suggest-payload-metro.types.ts';
+export * from './request/suggest-payload-mktu.types.ts';
+export * from './request/suggest-payload-okpd2.types.ts';
+export * from './request/suggest-payload-okved2.types.ts';
+export * from './request/suggest-payload-party.types.ts';
 export * from './request/suggest-payload-party_by.types.ts';
 export * from './request/suggest-payload-party_kz.types.ts';
-export * from './request/suggest-payload-party.types.ts';
+export * from './request/suggest-payload-postal_unit.types.ts';
+export * from './request/suggest-payload-region_court.types.ts';
+
 export * from './request/suggest-payload.types.ts';
 
 export * from './response/address.types.ts';
+export * from './response/bank.types.ts';
+export * from './response/car_brand.types.ts';
 export * from './response/countries.types.ts';
+export * from './response/currency.types.ts';
 export * from './response/email.types.ts';
+export * from './response/fias.types.ts';
 export * from './response/fio.types.ts';
+export * from './response/fms_unit.types.ts';
+export * from './response/fns_unit.types.ts';
+export * from './response/fts_unit.types.ts';
+export * from './response/metro.types.ts';
+export * from './response/mktu.types.ts';
+export * from './response/okpd2.types.ts';
+export * from './response/oktmo.types.ts';
+export * from './response/okved2.types.ts';
+export * from './response/party.types.ts';
 export * from './response/party_by.types.ts';
 export * from './response/party_kz.types.ts';
 export * from './response/postal_unit.types.ts';
+export * from './response/region_court.types.ts';
 
 export * from './response/suggestions.types.ts';
