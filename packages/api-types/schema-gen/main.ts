7b158a73fe9de903cec14a8def17bd1fb032d203
alexchexes <alexchexes@gmail.com>
2025-06-11 12:15:52 +0100

    chore(types): Use Prettier to prettify JSON-schema instead of JSON.stringify(..., null, 2)
    - reduce the number of lines in generated schemas by 40-55%

--- packages/api-types/schema-gen/main.ts
+++ packages/api-types/schema-gen/main.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import * as prettier from 'prettier';
 import { tsToSchema } from './util/tsToSchema';
 import { writeToFile } from './util/writeToFile';
 import { log } from './util/log';
@@ -13,13 +14,16 @@ function generateSchemasFromDirectory() {
 
   const files = fs.readdirSync(absoluteTypesDir).filter((file) => file.endsWith('.types.ts'));
 
-  files.forEach((file) => {
+  files.forEach(async (file) => {
     log(chalk.bgWhite.black(`\n Generating JSON-schema from file: ${TYPES_DIR}/${file} `));
 
     const fullPath = path.join(absoluteTypesDir, file);
 
     const schema = tsToSchema({ path: fullPath });
-    const schemaString = JSON.stringify(schema, null, 2);
+    const schemaString = await prettier.format(JSON.stringify(schema), {
+      parser: 'json',
+      printWidth: 120,
+    });
 
     const outputFileName = path.basename(file, '.types.ts');
     const outputFile = `${BASE_OUTPUT_DIR}/${outputFileName}.json`;
